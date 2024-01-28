3c4669a4cd79e7cc56d81ade39798d32cbef486d
sappho <sappho@sappho.io>
2024-01-28 09:51:52 -0500

    Update _github_actions_setup.sh

--- cicd/_github_actions_setup.sh
+++ cicd/_github_actions_setup.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 set -euxo pipefail
+
 pwd
 ls -la
+
 mkdir /accelerator
 cp -rT /accelerator "$(pwd)"
 cd /accelerator
+
 pwd
+ls -la
+
+
 bash ./cicd/_accelerator_docker_build_internal.sh
